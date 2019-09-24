d8572cc29882c01051b336ea79b6b15355d757eb
adamsaimi <adam000202@gmail.com>
2019-09-24 20:19:35 +0200

    added groupeTd as a parameter. It was requested in the readme

--- datamodel/src/main/java/fr/univparis/maljae/Student.java
+++ datamodel/src/main/java/fr/univparis/maljae/Student.java
@@ -5,15 +5,18 @@ import org.json.*;
 public class Student {
     private String  email;
     private Boolean confirmed;
+    private String groupeTd;//New adding
 
     Student (JSONObject json) {
+    groupeTd = groupeTd0;
 	email = json.getString ("email");
 	confirmed = json.getBoolean ("confirmed");
     }
 
-    Student (String email0, Boolean confirmed0) {
+    Student (String email0, Boolean confirmed0, String groupeTd0) {
 	email = email0;
 	confirmed = confirmed0;
+	groupeTd⁼groupeTd0;
     }
 
     public String getEmail () {
@@ -23,24 +26,29 @@ public class Student {
     public Boolean getConfirmed () {
 	return confirmed;
     }
+    
+    public String getGroupeTd(){//new Adding
+    return groupeTd;
+    }
 
     public void setConfirmed (Boolean b) {
 	confirmed = b;
     }
 
     public String toString () {
-	return email + "/" + confirmed;
+	return email + "/" + confirmed + "/" + groupeTd;
     }
 
     public static Student fromString (String s) {
 	String[] fields = s.split ("/");
-	return new Student (fields[0], Boolean.parseBoolean (fields[1]));
+	return new Student (fields[0], Boolean.parseBoolean (fields[1]),fields[2]);//expected to be a third field for groupeTd
     }
 
     public JSONObject toJSON () {
 	JSONObject json = new JSONObject ();
 	json.put ("email", email);
 	json.put ("confirmed", confirmed);
+	json.put("groupeTd",groupeTd);
 	return json;
     }
 
