a87db3163df7929a0b77bec45b86586399ef39ee
adam <adam000202@gmail.com>
2019-10-17 11:24:22 +0200

    Revert Student.java

--- datamodel/src/main/java/fr/univparis/maljae/Student.java
+++ datamodel/src/main/java/fr/univparis/maljae/Student.java
@@ -5,18 +5,15 @@ import org.json.*;
 public class Student {
     private String  email;
     private Boolean confirmed;
-    private String groupeTd;//New adding
 
     Student (JSONObject json) {
-    groupeTd = groupeTd0;
 	email = json.getString ("email");
 	confirmed = json.getBoolean ("confirmed");
     }
 
-    Student (String email0, Boolean confirmed0, String groupeTd0) {
+    Student (String email0, Boolean confirmed0) {
 	email = email0;
 	confirmed = confirmed0;
-	groupeTd⁼groupeTd0;
     }
 
     public String getEmail () {
@@ -26,29 +23,25 @@ public class Student {
     public Boolean getConfirmed () {
 	return confirmed;
     }
-    
-    public String getGroupeTd(){//new Adding
-    return groupeTd;
-    }
+
 
     public void setConfirmed (Boolean b) {
 	confirmed = b;
     }
 
     public String toString () {
-	return email + "/" + confirmed + "/" + groupeTd;
+	return email + "/" + confirmed + "/";
     }
 
     public static Student fromString (String s) {
 	String[] fields = s.split ("/");
-	return new Student (fields[0], Boolean.parseBoolean (fields[1]),fields[2]);//expected to be a third field for groupeTd
+	return new Student (fields[0], Boolean.parseBoolean (fields[1]));//expected to be a third field for groupeTd
     }
 
     public JSONObject toJSON () {
 	JSONObject json = new JSONObject ();
 	json.put ("email", email);
 	json.put ("confirmed", confirmed);
-	json.put("groupeTd",groupeTd);
 	return json;
     }
 
