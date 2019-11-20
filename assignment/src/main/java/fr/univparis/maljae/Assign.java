cb00be89ca77a0540fdfe47f4bdb0206f084662d
Theo Mauffret <theomauffret1S1@laposte.net>
2019-11-20 19:59:37 +0100

    I add AssignTask Method to assign to each team their final task

--- assignment/src/main/java/fr/univparis/maljae/Assign.java
+++ assignment/src/main/java/fr/univparis/maljae/Assign.java
@@ -28,4 +28,31 @@ public class Assign
 		}
 		return Numbers;
 	}
+
+  public static HashMap<Team, Task> AssignTask(Teams a)
+	{
+		ArrayList<Team> loc = SortingAlgo(a); // We create ArrayList with the final order of teams
+		HashMap<Team, Task> HashMapLocal = new HashMap<Team,Task>(); // We create local Hash map which will contain the final assignment
+		ArrayList<Task> takslock = null; // This is an arraylist created to contain all preference that are not available anymore
+
+		for(int i=0; i<loc.size();i++)
+		{
+			int compteur =0; // We add compteur to identify which preference task going to be assign for each team
+			while(takslock.contains(loc.get(i).getPreferences().get(compteur))) // While tasklock contain preference of the team
+			{
+				compteur+=1; // We add 1 to take the next preference of the team
+			}
+			HashMapLocal.put(loc.get(i),loc.get(i).getPreferences().get(compteur) ); //We add to HashMapLocal the final choose
+			takslock.add(loc.get(i).getPreferences().get(compteur)); // Ans we add to the taksLock ArrayList the preference that we just took
+		}
+
+		return HashMapLocal;
+	}
+
+
+
+
+
+
+
 }
