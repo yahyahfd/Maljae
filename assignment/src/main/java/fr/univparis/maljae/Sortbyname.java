133ab7fd9817e7bf13d4c3e541d5e3e83d75cf72
adamsaimi <adam000202@gmail.com>
2019-11-20 21:11:40 +0100

    Update Sortbyname.java

--- assignment/src/main/java/fr/univparis/maljae/Sortbyname.java
+++ assignment/src/main/java/fr/univparis/maljae/Sortbyname.java
@@ -1,5 +1,8 @@
 package fr.univparis.maljae.assignement;
 import fr.univparis.maljae.datamodel;
+import java.util.*; 
+import java.lang.*; 
+import java.io.*; 
 class Sortbyname implements Comparator<Student> 
 { 
     // Used for sorting in ascending order of 
