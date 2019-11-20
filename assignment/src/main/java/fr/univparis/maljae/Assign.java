2d540e06cd5f527f4a051fba9e5c85c67ebec45e
adam <adam000202@gmail.com>
2019-11-20 21:50:16 +0100

    I added the entire assign algo with three differents and also an interface to sort

--- assignment/src/main/java/fr/univparis/maljae/Assign.java
+++ assignment/src/main/java/fr/univparis/maljae/Assign.java
@@ -1,5 +1,8 @@
 package fr.univparis.maljae.assignement;
-import fr.univparis.maljae.datamodel.Configuration;
+import fr.univparis.maljae.datamodel.*;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 /**
  * Hello world!
@@ -11,4 +14,38 @@ public class Assign
     {
 	System.out.println("maljae assigner version " + Configuration.version + " is not implemented yet.");
     }
+
+    public static void alphabetical_order(Teams a)
+ {
+   Collections.sort(a.getTeams(),new SortbyMail());//we sort each team by alphabetical_order of their mail
+ }
+
+ public static void SortingAlgo(Teams a)
+ {
+   alphabetical_order(a);
+   for(int i=0; i<a.getTeams().size();i++)
+   {
+     Collections.swap(a.getTeams(), i, (i+(a.getTeams().get(i).getSeed()%(a.getTeams().size()-i)))-1);//here we do the swapping using the alghorithm
+   }
+ }
+
+ public static void AssignTask(Teams a)//we call all the sorting method in this and then assign each team a task
+ {
+   if(a.getTeams().size()==0)return;//just to check
+   SortingAlgo(a);
+   ArrayList<Task> tasks = a.getTeams().get(0).getPreferences(); // This is an arraylist created to contain all the task available
+
+   for(int i=0; i<a.getTeams().size();i++)
+   {
+     for (int j=0;j<a.getTeams().get(i).getPreferences().size();j++) {
+       if(tasks.contains(a.getTeams().get(i).getPreferences().get(j))){//if the task preferred is still available we can assign it and then delete the task frome the list
+         Assignment.assignTask(a.getTeams().get(i),a.getTeams().get(i).getPreferences().get(j));
+         tasks.remove(a.getTeams().get(i).getPreferences().get(j));
+         break;
+       }
+     }
+   }
+ }
+
+
 }
