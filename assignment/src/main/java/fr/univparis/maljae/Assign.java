7a1b10cba8eb00c38b6ddd693074deb8cacd59da
Theo Mauffret <theomauffret1S1@laposte.net>
2019-11-20 18:52:51 +0100

    Tranfer completed

--- assignment/src/main/java/fr/univparis/maljae/Assign.java
+++ assignment/src/main/java/fr/univparis/maljae/Assign.java
@@ -11,4 +11,21 @@ public class Assign
     {
 	System.out.println("maljae assigner version " + Configuration.version + " is not implemented yet.");
     }
+
+    public static String[] alphabetical_order(Teams a)
+	{
+		String[] TeamsNamesLocalVariable= a.getTeamsNames();
+	    Arrays.sort(TeamsNamesLocalVariable);
+	    return TeamsNamesLocalVariable;
+	}
+
+	public static ArrayList<Team> SortingAlgo(Teams a)
+	{
+		ArrayList<Team> Numbers= a.getTeams();
+		for(int i=0; i<Numbers.size();i++) // Browse all Arraylist element
+		{
+			Collections.swap(Numbers, i, (i+(Numbers.get(i).getSeed()%(Numbers.size()-i)))-1);
+		}
+		return Numbers;
+	}
 }
