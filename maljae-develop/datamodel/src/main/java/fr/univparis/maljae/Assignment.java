aaef7192e26f75d1585e0e102e726fbdf3c6a051
Rayane <rayane.amaury@gmail.com>
2019-11-30 17:34:56 +0100

    I did two tests : one in sendEmail() at Notifier.java and another one at updateStudents method at team.java

---
+++ maljae-develop/datamodel/src/main/java/fr/univparis/maljae/Assignment.java
@@ -0,0 +1,77 @@
+package fr.univparis.maljae;
+
+import java.util.regex.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.io.*;
+import java.util.*;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+import org.apache.commons.io.FileUtils;
+import org.json.*;
+
+/*** This module stores tasks assignment. */
+public class Assignment {
+
+    /** For each team, there must be exactly one task. */
+    private static HashMap<Team, Task> team_tasks = new HashMap<Team, Task> ();
+
+    /** We must provide a trace of the assignment algorithm. */
+    private static String trace;
+
+    /** Returns a string representation for the trace. */
+    public static String getTrace () { return trace; }
+
+    public static void addTraceStep (String s) { trace += s + "\n"; }
+
+    public static void assignTask (Team team, Task task) {
+	if (team_tasks.containsKey (team)) {
+	    throw new RuntimeException ("Team " + team.getIdentifier ()
+					+ " already has a task.");
+	}
+	team_tasks.put (team, task);
+	addTraceStep (team.getIdentifier ()
+		      + " is assigned task "
+		      + task.getIdentifier ());
+    }
+
+    public static String show () {
+	String description = "";
+	description += team_tasks.toString () + "\n";
+	description += trace;
+	return description;
+    }
+
+    public static void loadFrom (File f) throws IOException {
+	JSONObject json = new JSONObject (FileUtils.readFileToString (f, "utf-8"));
+	trace = json.getString ("trace");
+	JSONArray assignment_json = json.getJSONArray ("assignment");
+	for (int i = 0; i < assignment_json.length (); i++) {
+	    JSONArray team_task = assignment_json.getJSONArray (i);
+	    Team team = Teams.getTeam (team_task.getString (0));
+	    Task task = Configuration.getTask (team_task.getString (1));
+	    assignTask (team, task);
+	}
+    }
+
+    public static void saveTo (File f) throws IOException {
+	JSONObject json = new JSONObject ();
+	json.put ("trace", trace);
+	JSONArray assignment_json = new JSONArray ();
+	for (HashMap.Entry<Team, Task> assignment : team_tasks.entrySet ()) {
+	    JSONArray team_task = new JSONArray ();
+	    team_task.put (assignment.getKey ().getIdentifier ());
+	    team_task.put (assignment.getValue ().getIdentifier ());
+	}
+	json.put ("assignment", assignment_json);
+	FileWriter fw = new FileWriter (f);
+	fw.write (json.toString (2));
+	fw.close ();
+    }
+	public static void main(String[]args){
+		Student s1=new Student("student.mail@gmail.com",true);
+		Team t=new Team(s1);
+		t.updateStudentsFromString("student.mail@gmail.com","NewStudent");
+		
+	}
+
+}
---
+++ maljae-develop/webserver/src/main/java/fr/univparis/maljae/Notifier.java
@@ -0,0 +1,65 @@
+package fr.univparis.maljae.webserver;
+import fr.univparis.maljae.datamodel.*;
+import java.io.*;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class Notifier {
+    public static void sendEmail (String email, String subject, String messageSend)throws IOException{
+      String username = "maljae.sendmail@gmail.com";
+      String password = "maljae123";
+      Properties prop = new Properties();
+  		prop.put("mail.smtp.host", "smtp.gmail.com");
+      prop.put("mail.smtp.port", "465");
+      prop.put("mail.smtp.auth", "true");
+      prop.put("mail.smtp.socketFactory.port", "465");
+      prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+      Session session = Session.getInstance(prop,new javax.mail.Authenticator() {
+        protected PasswordAuthentication getPasswordAuthentication() {
+          return new PasswordAuthentication(username,password);
+        }
+      });
+      try {
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress("maljae.sendmail@gmail.com"));
+        message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(email));
+        message.setSubject(subject);
+        message.setText(messageSend);
+        Transport.send(message);
+      } catch (MessagingException e) {
+        e.printStackTrace();
+      }
+    }
+
+    public static void sendTeamCreation (String host, Token token)
+	throws IOException
+    {
+	String message =
+	    "Hello!\n" +
+	    "You have requested the creation of a team.\n" +
+	    "Here are the links to perform actions on this team.\n" +
+	    "- To edit your team information :\n" +
+	    "  http://" + host + "/team/edit/" + token.toString () + "\n" +
+	    "- To delete your team :\n" +
+	    "  http://" + host + "/team/delete/" + token.toString () + "\n";
+	sendEmail (token.getEmail (), "[maljae] Team Creation", message);
+    }
+    public static void sendUpdate(String host,Token token,String who)throws IOException{
+      String message="Hello!\n" +
+  	    who+" have edited the team.\n" +
+  	    "Here are the links to perform actions on this team.\n" +
+  	    "- To edit your team information :\n" +
+  	    "  http://" + host + "/team/edit/" + token.toString () + "\n" +
+  	    "- To delete your team :\n" +
+  	    "  http://" + host + "/team/delete/" + token.toString () + "\n";
+      for(Student student:token.getTeam().getStudents()){
+        sendEmail(student.getEmail(),"[maljae] Team Updated",message);
+      }
+    }
+	public static void main(String[]args){
+		sendEmail("rayane.amaury@gmail.com"," test ","I'm sending my self a message to see if the method works");
+	}
+}
