e39ef0c1c23fffcd9293d5762dc0ae6b6c7dad4f
Hafid Yahya <hafidyahya1999@gmail.com>
2019-12-18 08:50:27 +0100

    changed Student.java

--- datamodel/src/main/java/fr/univparis/maljae/Student.java
+++ datamodel/src/main/java/fr/univparis/maljae/Student.java
@@ -7,58 +7,70 @@ public class Student {
     private Boolean confirmed;
     private int groupeTd;
 
-    Student (JSONObject json) {      //Json contructor for student
-	email = json.getString ("email");
-	confirmed = json.getBoolean ("confirmed");
-  if (json.has("groupeTd")) groupeTd = json.getInt("groupeTd");
+    /** Json contructor for student. */
+    Student (JSONObject json) {
+    	email = json.getString ("email");
+    	confirmed = json.getBoolean ("confirmed");
+      if (json.has("groupeTd")) groupeTd = json.getInt("groupeTd");
     }
 
-    Student (String email0, Boolean confirmed0) {        //Simple constructor for student without specified TD group
-	email = email0;
-	confirmed = confirmed0;
+    /** Simple constructor for student without specified TD group. */
+    Student (String email0, Boolean confirmed0) {
+    	email = email0;
+    	confirmed = confirmed0;
     }
-    Student (String email0, Boolean confirmed0,int groupeTd0) {      //Same constructor with a specified TD group
- email = email0;
- confirmed = confirmed0;
- groupeTd = groupeTd0;
+
+    /** Simple constructor for student with a specified TD group. */
+    Student (String email0, Boolean confirmed0,int groupeTd0) {
+     email = email0;
+     confirmed = confirmed0;
+     groupeTd = groupeTd0;
    }
 
+    /** Returns the current student's email. */
     public String getEmail () {
-	return email;
+    	return email;
     }
 
+    /** Returns the current student's status, whether it is confirmed that he is registered in a team or not. */
     public Boolean getConfirmed () {
-	return confirmed;
+	     return confirmed;
     }
 
+    /** Returns the curent student's group number. */
     public int getGroupeTd(){
-  return groupeTd;
+      return groupeTd;
     }
 
+    /** Changes the current student's status to confirmed if b equals true, or not confirmed if b equals false. */
     public void setConfirmed (Boolean b) {
-	confirmed = b;
+    	confirmed = b;
     }
 
+    /** Changes the current student's group number to g. */
     public void setGroupeTd(int g){
- groupeTd=g;
-   }
+      groupeTd=g;
+    }
 
+    /** Changes the toString Method so that it returns a string that shows the current student's email, status and group number. */
     public String toString () {
-  if(groupeTd>0&&groupeTd<5)return  email + "/" + confirmed + "/"+String.valueOf(groupeTd);
-	return email + "/" + confirmed + "/";
+      if(groupeTd>0 && groupeTd<5) return  email + "/" + confirmed + "/"+String.valueOf(groupeTd);
+    	return email + "/" + confirmed + "/";
     }
 
-    public static Student fromString (String s) {            //Creating a student from a string entry
-	String[] fields = s.split ("/");
-	return new Student (fields[0], Boolean.parseBoolean (fields[1]));
+    /** Creating a student from a string entry. */
+    public static Student fromString (String s) {
+    	String[] fields = s.split ("/");
+    	return new Student (fields[0], Boolean.parseBoolean (fields[1]));
     }
 
-    public JSONObject toJSON () {          //Converting into json file
-	JSONObject json = new JSONObject ();
-	json.put ("email", email);
-	json.put ("confirmed", confirmed);
-  if(groupeTd>0&&groupeTd<5) json.put("groupeTd",groupeTd);
-	return json;
+    /** Returns a JSONObject containing the current student's email, status and group number. */
+    public JSONObject toJSON () {
+    	JSONObject json = new JSONObject ();
+    	json.put ("email", email);
+    	json.put ("confirmed", confirmed);
+      if(groupeTd>0 && groupeTd<5) json.put("groupeTd",groupeTd);
+    	return json;
     }
 
 }
