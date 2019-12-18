69107ce6012bd4d66bbb6cd458a02cbf974dc957
Hafid Yahya <hafidyahya1999@gmail.com>
2019-12-18 08:51:39 +0100

    changed Assignment.java

--- datamodel/src/main/java/fr/univparis/maljae/Assignment.java
+++ datamodel/src/main/java/fr/univparis/maljae/Assignment.java
@@ -9,7 +9,7 @@ import java.text.DateFormat;
 import org.apache.commons.io.FileUtils;
 import org.json.*;
 
-/*** This module stores tasks assignment. */ 
+/*** This module stores tasks assignment. */
 public class Assignment {
 
     /** For each team, there must be exactly one task. */
@@ -21,6 +21,7 @@ public class Assignment {
     /** Returns a string representation for the trace. */
     public static ArrayList<String>  getTrace () { return trace; }
 
+    /** Adds a string to trace after removing all the specified characters inside replaceAll. */
     public static void addTraceStep (String key,String value){
       if(key==null||value==null||key.length()<1)return;
       String s=key+" : "+value;
@@ -28,47 +29,50 @@ public class Assignment {
       trace.add(s);
     }
 
-    public static void assignTask (Team team, Task task) {           //Assign a task to a team if it doesn't have any yet
-	if (team_tasks.containsKey (team)) {
-	    throw new RuntimeException ("Team " + team.getIdentifier ()
-					+ " already has a task.");
-	}
-	team_tasks.put (team, task);         //Adding the team with a task in the collection
+    /** Assign a task to a team if it doesn't have any yet. */
+    public static void assignTask (Team team, Task task) {
+    	if (team_tasks.containsKey (team)) {
+    	    throw new RuntimeException ("Team " + team.getIdentifier () + " already has a task.");
+    	}
+    	team_tasks.put (team, task);
     }
 
-    public static String show () {       //Showing a Team with the task and the trace
-	String description = "";
-	description += team_tasks.toString () + "\n";
-	description += trace;
-	return description;
+    /** Returns a string representation for each team and its assigned task then the trace. */
+    public static String show () {
+    	String description = "";
+    	description += team_tasks.toString () + "\n";
+    	description += trace;
+    	return description;
     }
 
-    public static void loadFrom (File f) throws IOException {                     //We load the file where we are stocking each team and the task that was assigned to it
-	JSONObject json = new JSONObject (FileUtils.readFileToString (f, "utf-8"));
-  JSONArray a=json.getJSONArray("trace");
-  for(int i=0;i<a.length();i++){
-    trace.add(a.optString(i));
-  }
+    /** We load the file f where we are stocking each team and the task that was assigned to it. */
+    public static void loadFrom (File f) throws IOException {
+    	JSONObject json = new JSONObject (FileUtils.readFileToString (f, "utf-8"));
+      JSONArray a=json.getJSONArray("trace");
+      for(int i=0;i<a.length();i++){
+        trace.add(a.optString(i));
+      }
     }
 
-    public static void saveTo (File f) throws IOException {            //We save the team and the task assigned in a file
-	JSONObject json = new JSONObject ();
-	JSONArray assignment_json = new JSONArray ();
-	for (HashMap.Entry<Team, Task> assignment : team_tasks.entrySet ()) {
-	    JSONArray team_task = new JSONArray ();
-	    team_task.put (assignment.getKey ().getIdentifier ());
-	    team_task.put (assignment.getValue ().getIdentifier ());
-      assignment_json.put(team_task);
-	}
-	JSONArray trace_json = new JSONArray ();
-	for (String  tr: trace) {
-      trace_json.put(tr);
-	}
-  json.put ("trace", trace_json);
-	json.put ("assignment", assignment_json);
-	FileWriter fw = new FileWriter (f);
-	fw.write (json.toString (2));
-	fw.close ();
+    /** We save the team and the task assigned inside the file f. */
+    public static void saveTo (File f) throws IOException {
+    	JSONObject json = new JSONObject ();
+    	JSONArray assignment_json = new JSONArray ();
+    	for (HashMap.Entry<Team, Task> assignment : team_tasks.entrySet ()) {
+    	    JSONArray team_task = new JSONArray ();
+    	    team_task.put (assignment.getKey ().getIdentifier ());
+    	    team_task.put (assignment.getValue ().getIdentifier ());
+          assignment_json.put(team_task);
+    	}
+    	JSONArray trace_json = new JSONArray ();
+    	for (String  tr: trace) {
+          trace_json.put(tr);
+    	}
+      json.put ("trace", trace_json);
+    	json.put ("assignment", assignment_json);
+    	FileWriter fw = new FileWriter (f);
+    	fw.write (json.toString (2));
+    	fw.close ();
     }
 
 }
