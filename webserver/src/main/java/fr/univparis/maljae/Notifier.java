c1b2aead67d4223dfa738205f0a227ffccf91b53
Hafid Yahya <hafidyahya1999@gmail.com>
2019-12-19 15:54:36 +0100

    I styled the pages needed and removed a method in Teams.java which used to try to delete a team if it existed before recreating it (?) and removed two lines in Notifier.java that used to add a useless link to delete the current team, to the mail sent. Nothing was changed in TeamController.java.

--- webserver/src/main/java/fr/univparis/maljae/Notifier.java
+++ webserver/src/main/java/fr/univparis/maljae/Notifier.java
@@ -42,9 +42,7 @@ public class Notifier {
 	    "You have requested the creation of a team.\n" +
 	    "Here are the links to perform actions on this team.\n" +
 	    "- To edit your team information :\n" +
-	    "  http://" + host + "/team/edit/" + token.toString () + "\n" +
-	    "- To delete your team :\n" +
-	    "  http://" + host + "/team/delete/" + token.toString () + "\n";
+	    "  http://" + host + "/team/edit/" + token.toString () + "\n";
 	sendEmail (token.getEmail (), "[maljae] Team Creation", message);
     }
     public static void sendUpdate(String host,Token token,String who)throws IOException{
@@ -52,9 +50,7 @@ public class Notifier {
   	    who+" have edited the team.\n" +
   	    "Here are the links to perform actions on this team.\n" +
   	    "- To edit your team information :\n" +
-  	    "  http://" + host + "/team/edit/" + token.toString () + "\n" +
-  	    "- To delete your team :\n" +
-  	    "  http://" + host + "/team/delete/" + token.toString () + "\n";
+  	    "  http://" + host + "/team/edit/" + token.toString () + "\n";
       for(Student student:token.getTeam().getStudents()){
         sendEmail(student.getEmail(),"[maljae] Team Updated",message);
       }
