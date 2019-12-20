01b1f1721620424d9a0641d56f664fc773e5ed39
Hafid Yahya <hafidyahya1999@gmail.com>
2019-12-20 06:56:20 +0100

    Modified what was needed in Teams.java so that in the createTeam method, it checks first that the email used in the parameter is not being used by any other team, the method now returns null if the email is being used, or the newTeam created if the email was never used before. I added the html page team-creation-impossible.html which shows in case the team cannot be created (email already used). I also made changes accordingly in TeamController.java so that the intallTeamCreate method catches the NullPointerException sent by the createTeam method in Teams.java. I finally added an exception catcher in the installTeamEdit method so that it catches an error incase we ever try to edit a non-existing team. Same thing for installConfirmationUpdate and InstallTeamUpdate.

--- webserver/src/main/java/fr/univparis/maljae/TeamController.java
+++ webserver/src/main/java/fr/univparis/maljae/TeamController.java
@@ -53,44 +53,50 @@ public class TeamController {
 
     public static void installTeamUpdate (Javalin app) {
       app.post("/team/update/:token", ctx -> {
-        Token token = Token.getToken (ctx.pathParam ("token"));
-        String who = token.getEmail ();
-        String teamName = token.getTeam ().getIdentifier ();
-        Team team = Teams.getTeam (teamName);
-        String host = ctx.host ();
-        team.updateSecretFromString (ctx.formParam ("secret"));
-        team.updatePreferencesFromString (ctx.formParam ("preferences"));
-        Teams.saveTeam (team);
-        if(team.who(ctx.formParam ("students"))!=null){
-          Edit editToConfirm=team.who(ctx.formParam ("students"));
-          Notifier.sendTeamEditConfirm(host,token,editToConfirm);
+        try{
+          Token token = Token.getToken (ctx.pathParam ("token"));
+          String who = token.getEmail ();
+          String teamName = token.getTeam ().getIdentifier ();
+          Team team = Teams.getTeam (teamName);
+          String host = ctx.host ();
+          team.updateSecretFromString (ctx.formParam ("secret"));
+          team.updatePreferencesFromString (ctx.formParam ("preferences"));
+          Teams.saveTeam (team);
+          if(team.who(ctx.formParam ("students"))!=null){
+            Edit editToConfirm=team.who(ctx.formParam ("students"));
+            Notifier.sendTeamEditConfirm(host,token,editToConfirm);
+          }
+          Notifier.sendUpdate(host,token,who);
+        }catch(Exception e){
+          ctx.redirect("/team-update-done.html");
         }
-        Notifier.sendUpdate(host,token,who);
-        ctx.redirect("/team-update-done.html");
       });
     }
 
     public static void installConfirmationUpdate(Javalin app){
       app.get("/team/update-confirmed/:token/:student/:action",ctx ->{
-      Token token = Token.getToken (ctx.pathParam ("token"));
-      boolean act=Boolean.valueOf(ctx.pathParam ("action"));
-      String st=ctx.pathParam ("student");
-      String teamName = token.getTeam ().getIdentifier ();
-      Team team = Teams.getTeam (teamName);
-        try {
-          team.updateStudentsFromString(st,act);//true if added or false if deleted
-          if(team.studentsToString().length()!=0){
-            Teams.saveTeam (team);
-          }else{
-            File f = new File (Configuration.getDataDirectory ()+ "/token" + token + ".json");
-            Token.deleteTokenfile(f);
-          }
-        }catch(ErrorNotOnThisTeamAnymore e) {
-          ctx.redirect("/team-update-errorDel.html");
-        }catch(ErrorAlreadyOnThisTeam ee){
-          ctx.redirect("/team-update-errorAdd.html");
-        }
-        ctx.redirect("/team-update-done.html");
+        try{
+          Token token = Token.getToken (ctx.pathParam ("token"));
+          boolean act=Boolean.valueOf(ctx.pathParam ("action"));
+          String st=ctx.pathParam ("student");
+          String teamName = token.getTeam ().getIdentifier ();
+          Team team = Teams.getTeam (teamName);
+            try {
+              team.updateStudentsFromString(st,act);//true if added or false if deleted
+              if(team.studentsToString().length()!=0){
+                Teams.saveTeam (team);
+              }else{
+                File f = new File (Configuration.getDataDirectory ()+ "/token" + token + ".json");
+                Token.deleteTokenfile(f);
+              }
+            }catch(ErrorNotOnThisTeamAnymore e) {
+              ctx.redirect("/team-update-errorDel.html");
+            }catch(ErrorAlreadyOnThisTeam ee){
+              ctx.redirect("/team-update-errorAdd.html");
+            }
+        }catch(Exception eee){
+          ctx.redirect("/team-update-done.html");
+        }        
       });
     }
 
