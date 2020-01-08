3cc8f8b0ac41e25c41a4cd508d84c725adbe8491
Hafid Yahya <hafidyahya1999@gmail.com>
2020-01-08 17:17:33 +0100

    Made few changes to fix a small error on deleting a team member

--- datamodel/src/main/java/fr/univparis/maljae/ErrorSamePreferences.java
+++
@@ -1,9 +0,0 @@
-package fr.univparis.maljae.datamodel;
-public class ErrorSamePreferences extends RuntimeException{
-  public ErrorSamePreferences(){
-    super();
-  }
-  public String toString(){
-    return "No changes were made to preferences.";
-  }
-}
